1487b4d2af2cdc79f4e33151a237163bc6f4834f
edwardlando <edwardlando8@gmail.com>
2013-07-09 15:02:04 -0700

    created users with fb login, items, and groups

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Market::Application.config.secret_token = 'ec7eb1089e40b6c15005332836cfd488745ebe7a89df6b87ca14f160b9587f8ef90c3703c96c6fed08b046121e92f74e8c7f15806d6afc9cbed0760a0378a4f0'
